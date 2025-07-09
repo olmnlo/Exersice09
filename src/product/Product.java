47e3de63b8501e7560e17c2dbd3c0681653e1c66
olmnlo <hosamag54@gmail.com>
2025-07-09 13:36:50 +0300

    done Product class

---
+++ src/product/Product.java
@@ -0,0 +1,38 @@
+package product;
+
+public abstract class Product {
+    private String name;
+    private double price;
+
+
+    public Product(){
+
+    }
+
+    public Product(String name, double price){
+        this.name = name;
+        setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if(price < 0){
+            throw new RuntimeException("you can not enter negative number");
+        }
+        this.price = price;
+    }
+
+    public abstract double getDiscount();
+
+}
